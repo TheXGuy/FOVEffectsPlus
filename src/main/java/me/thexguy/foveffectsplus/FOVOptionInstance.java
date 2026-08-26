148ef38e4d943e25c65e2fe3a92ccc95aec1364e
TheXGuy <superdoublex1@gmail.com>
2026-08-26 12:11:49 -0400

    26.1

--- src/main/java/me/thexguy/foveffectsplus/FOVOptionInstance.java
+++ src/main/java/me/thexguy/foveffectsplus/FOVOptionInstance.java
@@ -6,11 +6,13 @@ import me.thexguy.foveffectsplus.config.ModConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.OptionInstance;
 import net.minecraft.client.Options;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.network.chat.Component;
 
 import java.awt.*;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class FOVOptionInstance {
     private static final ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
@@ -47,7 +49,13 @@ public class FOVOptionInstance {
             }
 
             return var10000;
-        }, new OptionInstance.IntRange(config.globalOptions.minFOV, config.globalOptions.maxFOV), Codec.DOUBLE.xmap((value) -> (int)(value * (double)40.0F + (double)70.0F), (value) -> ((double)value - (double)70.0F) / (double)40.0F), currentFOV, OptionInstance.NO_ACTION);
+        }, new OptionInstance.IntRange(config.globalOptions.minFOV, config.globalOptions.maxFOV), Codec.DOUBLE.xmap((value) -> (int)(value * (double)40.0F + (double)70.0F), (value) -> ((double)value - (double)70.0F) / (double)40.0F), currentFOV, (value) -> operateOnLevelRenderer(LevelRenderer::needsUpdate));
         return fov;
     }
+
+    private static void operateOnLevelRenderer(final Consumer<LevelRenderer> consumer) {
+        LevelRenderer levelRenderer = Minecraft.getInstance().levelRenderer;
+        consumer.accept(levelRenderer);
+    }
+
 }
